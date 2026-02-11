9165546a8745381bca7fe14d6c2e9d383c618777
Jim Hayes <spinaltap12345@gmail.com>
2026-02-11 13:35:16 -0800

    Update copyright.

--- PHB5E.js
+++ PHB5E.js
@@ -1,5 +1,5 @@
 /*
-Copyright 2025, James J. Hayes
+Copyright 2026, James J. Hayes
 
 This program is free software; you can redistribute it and/or modify it under
 the terms of the GNU General Public License as published by the Free Software
